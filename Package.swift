380be4be6d846650a61741bb05c5cd1f47acc4b4
joekndy <joe@ml.to>
2020-10-12 12:07:17 -0700

    supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MarqueeText",
+    platforms: [
+         SupportedPlatform.iOS(.v13),
+         SupportedPlatform.macOS(.v10_15)
+     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
