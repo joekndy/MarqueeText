53cb79bf9073a29043a26aca95ac48d65da3271e
Nasir Mehmood <nasir@csms.ae>
2023-03-28 00:21:25 +0400

    tvOS check added

--- Sources/MarqueeText/SwiftUI+Extension.swift
+++ Sources/MarqueeText/SwiftUI+Extension.swift
@@ -13,7 +13,7 @@ import Combine
 extension View {
     /// A backwards compatible wrapper for iOS 14 `onChange`
     @ViewBuilder func onValueChanged<T: Equatable>(of value: T, perform onChange: @escaping (T) -> Void) -> some View {
-        if #available(iOS 14.0, *) {
+        if #available(iOS 14.0, *), #available(tvOS 14.0, *) {
             self.onChange(of: value, perform: onChange)
         } else {
             self.onReceive(Just(value)) { (value) in
